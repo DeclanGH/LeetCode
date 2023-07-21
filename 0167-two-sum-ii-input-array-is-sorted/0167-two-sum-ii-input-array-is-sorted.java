28e50292e5ec36335ba83abe5ad060522d45e5de
Declan Onunkwo <115195431+DeclanGH@users.noreply.github.com>
2023-07-21 09:52:44 -0400

    Time: 1 ms (99.29%), Space: 45.5 MB (72.46%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
@@ -1,6 +1,5 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int[] res = new int[2];
         int i = 0;
         int j = nums.length-1;
         while(true){ // Because no edge case. There must be a result
