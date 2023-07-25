ddc4e05f6f278691bb3dd4af853b400d2c7450a1
Declan Onunkwo <115195431+DeclanGH@users.noreply.github.com>
2023-07-24 20:44:00 -0400

    Time: 1 ms (99.62%), Space: 46.5 MB (51.38%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.java
+++ 0338-counting-bits/0338-counting-bits.java
@@ -1,18 +1,14 @@
 class Solution {
-    public int[] countBits(int n) {
-        int[] bitCount = new int[n+1];
-        int count = n;
-        while(count != 0){ // This is O(n)
-            int ones = 0;
-            n = count;
-            while(n != 0){ // This is an O(1) while loop
-                n &= (n-1);
-                n >>>= 1;
-                ones += 1;
-            }
-            bitCount[count] = ones;
-            count--;
+    public int[] countBits(int n) { // a better O(n) solution?
+        int[] dp = new int[n+1];
+        dp[0] = 0; // base
+        int i = 1;
+        int offset = 1;
+        while(i <= n){ // its weird but works.
+            dp[i] = 1 + dp[i-offset];
+            i++;
+            offset = (i%offset==0) ? offset *= 2 : offset;
         }
-        return bitCount; // Therefore this solution is O(n)
+        return dp;
     }
 }
