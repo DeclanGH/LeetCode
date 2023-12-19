ef7ad46980a94d8e1f693461bb79342dc097dc6c
Declan Onunkwo <115195431+DeclanGH@users.noreply.github.com>
2023-12-18 22:55:57 -0500

    Time: 0 ms (100.00%), Space: 40.7 MB (24.27%) - LeetHub

--- 0413-arithmetic-slices/0413-arithmetic-slices.java
+++ 0413-arithmetic-slices/0413-arithmetic-slices.java
@@ -1,15 +1,10 @@
 class Solution {
     public int numberOfArithmeticSlices(int[] nums) {
         
-        if(nums.length < 3){
-            return 0;
-        }
+        if (nums.length < 3) return 0;
         
         int subarrayCount = 0;
-        
-        PriorityQueue<Integer> countsAscending = new PriorityQueue<>((a, b) -> Integer.compare(a, b));
-        HashMap<Integer, Integer> countMultiplier = new HashMap<>();
-        
+                
         int i = 0;
         
         while (true) {
@@ -25,28 +20,11 @@ class Solution {
             
             int subCount = count - 1;
             
-            if (!countMultiplier.containsKey(subCount) && count >= 2) {
-                countsAscending.add(subCount);
-                countMultiplier.put(subCount, 1);
-            } else if (count >= 2) {
-                countMultiplier.put(subCount, countMultiplier.get(subCount) + 1);
-            }    
+            if (count >= 2) subarrayCount += (int) ((subCount/2.0) * (2 + (subCount-1)));
             
             if (j >= nums.length) break;
         }
-        
-        int itr = 1;
-        int currTotal = 0;
-            
-        while (!countsAscending.isEmpty()) {
-            int currCount = countsAscending.poll();
-            while (itr <= currCount) {
-                currTotal += itr;
-                itr++;
-            }
-            subarrayCount += currTotal * countMultiplier.get(currCount);
-        }
-        
+
         return subarrayCount;
     }
 }
