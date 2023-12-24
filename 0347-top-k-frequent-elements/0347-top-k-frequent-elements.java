613a036cb672782810fbe39d430f4650e4029e50
Declan Onunkwo <115195431+DeclanGH@users.noreply.github.com>
2023-12-24 04:11:21 -0500

    Time: 16 ms (23.32%), Space: 48.3 MB (6.37%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,33 +1,23 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
         if (nums.length == k) return nums;
+
         int[] result = new int[k];
         HashMap<Integer,Integer> map = new HashMap<>();
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.comparingInt(map::get).reversed());
 
         for(int n : nums){
             map.put(n, map.getOrDefault(n,0)+1);
         }
 
-        ArrayList<Integer> val = new ArrayList<>();
-        for(Map.Entry<Integer,Integer> entry : map.entrySet()){
-            val.add(entry.getValue());
-        }
-        Collections.sort(val);
+        pq.addAll(map.keySet());
 
-        int i = 0;
-        for(int j = val.size()-1; j>=0; j--){
-            for(Map.Entry<Integer,Integer> entry : map.entrySet()){
-                if(entry.getValue() == val.get(j)){
-                    result[i] = entry.getKey();
-                    map.remove(result[i]);
-                    i++;
-                    break;
-                }
-            }
-            if(i == k){
-                break;
-            }
+        int counter = 0;
+        while (counter < k) {
+            result[counter] = pq.poll();
+            counter++;
         }
+
         return result;
     }
 }
